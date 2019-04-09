29a0e7723601b6df47d83e04648770ff025feb73
Josef Strzibny <strzibny@strzibny.name>
2019-04-08 19:20:28 -0500

    Use fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
